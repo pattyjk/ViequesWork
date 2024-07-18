59ff40872b58cd2522ba178ed4d6bc1fa02aead9
Patrick Kearns <kearnspj23@gmail.com>
2024-07-17 20:26:41 -0400

    add

--- vieques_soil_analysis.R
+++ vieques_soil_analysis.R
@@ -41,7 +41,7 @@ ko.coords<-merge(ko.coords, meta, by.x='SampleID', by.y='SampleID')
 #7.8
 
 #plot PCoA
-ggplot(ko.coords, aes(MDS1, MDS2, color=PlantSpecies, label=SampleID))+
+ggplot(ko.coords, aes(MDS1, MDS2, color=PlantSpecies, shape=Soil, label=SampleID))+
   geom_point(size=3.5)+
   #geom_text()+
   theme_bw()+
@@ -49,6 +49,30 @@ ggplot(ko.coords, aes(MDS1, MDS2, color=PlantSpecies, label=SampleID))+
   xlab("PC1- 27.4%")+
   ylab("PC2- 7.8%")
 
+
+#calculate PERMANOVA with adonis2
+s16.dis<-as.data.frame(as.matrix(vegdist(t(asv_table), method='bray')))
+s16.dis2<-s16.dis
+s16.dis2$SampleID<-row.names(s16.dis2)
+
+#select only columns of interest for the metadata
+meta2<-meta[,c(1,5,8)]
+
+#add metadata 
+s16.dis3<-merge(s16.dis2, meta2, by='SampleID')
+
+#remove sample id column
+s16.dis4<-s16.dis3[,-1]
+
+#calculate adonis
+set.seed(4)
+adonis2(s16.dis4[,-c(52,53)] ~ PlantSpecies + Soil, data=s16.dis4, permutations = 10000)
+
+#             Df SumOfSqs      R2       F    Pr(>F)    
+#PlantSpecies  8  0.51920 0.77545 19.7213 9.999e-05 ***
+#Soil          1  0.01542 0.02303  4.6851    0.051   
+#Residual     41  0.13492 0.20152                      
+#Total        50  0.66954 1.00000                      
 
 ###Calculate alpha diversity
 
@@ -84,3 +108,9 @@ ggplot(larv.alph, aes(PlantSpecies, Shannon, fill=PlantSpecies))+
   xlab("")+
   coord_flip()+
   ylab("Shannon Diversity")
+
+pairwise.t.test(larv.alph$Shannon, larv.alph$Soil)
+#             Composta
+#La Semillera 0.092
+
+pairwise.t.test(larv.alph$Shannon, larv.alph$PlantSpecies)
