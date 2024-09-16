7a25b9a193ec1dd347e970b4df81d8a95cd7a8e9
Patrick Kearns <kearnspj23@gmail.com>
2024-09-16 11:52:57 -0400

    fixing errors in naming

--- Soil/vieques_soil_analysis.R
+++ Soil/vieques_soil_analysis.R
@@ -31,7 +31,6 @@ ggplot(vieq.alph, aes(PlantSpecies, `diversity(otu_rare, index = "shannon")`, fi
 ##########################################################################
 ############################Beta diversity################################
 ##########################################################################
-
 #calculate beta diversity
 ko_pcoa<-capscale(otu_rare  ~ 1, distance='bray')
 
@@ -49,17 +48,63 @@ ko.coords<-merge(ko.coords, meta, by.x='SampleID', by.y='SampleID')
 
 #calculate percent variation explained for first two axis
 100*round(ko_pcoa$CA$eig[1]/sum(ko_pcoa$CA$eig), 3)
-#30
+#29.9
 100*round(ko_pcoa$CA$eig[2]/sum(ko_pcoa$CA$eig), 3)
-#12.1
+#12.2
 
-#color=Time,
 
-#plot PCoA
-ggplot(ko.coords, aes(MDS1, MDS2, shape=PlantSpecies, color=Soil, label=SampleID))+
- # geom_point(size=2.7)+
+
+ggplot(ko.coords, aes(MDS1, MDS2, color=PlantSpecies, label=SampleID, shape=Soil))+
+  #geom_point(size=2.7)+
   geom_text()+
   theme_bw()+
   ylab("PC2- 12.1%")+
   guides(alpha = "none")+
   xlab("PC1- 30.0%")
+
+#plot PCoA
+ggplot(ko.coords[which(ko.coords$Soil == 'Composta'),], aes(MDS1, MDS2, color=PlantSpecies, label=SampleID))+
+ # geom_point(size=2.7)+
+  geom_text()+
+  theme_bw()+
+  ylab("PC2- 12.1%")+
+  guides(alpha = "none")+
+  xlab("PC1- 30.0%")+
+  ylim(c(-0.5,.25))+
+  xlim(c(-.75,0))
+
+ggplot(ko.coords[-which(ko.coords$Soil == 'Composta'),], aes(MDS1, MDS2, color=PlantSpecies, label=SampleID))+
+  # geom_point(size=2.7)+
+  geom_text()+
+  theme_bw()+
+  ylab("PC2- 12.1%")+
+  guides(alpha = "none")+
+  xlab("PC1- 30.0%")+
+  xlim(c(1,1.5))+
+  ylim(c(-.75,-1.25))
+
+#adonis time, La Sem only
+meta_composta<-meta[-which(meta$Soil == 'Composta'),]
+meta_composta<-meta_composta[-c(1:2),]
+otu_rare2<-otu_rare[row.names(otu_rare) %in% meta_composta$SampleID,]
+
+larv.dis<-vegdist(otu_rare2, method='bray')
+larv.dis<-as.data.frame(as.matrix(larv.dis))
+larv.dis$SampleID<-row.names(larv.dis)
+larv.dis<-merge(larv.dis, meta_composta[,c(1,5)], by= 'SampleID', all.y=F)
+larv.dis<-larv.dis[,-1]
+larv.ad<-adonis(larv.dis[,-12] ~ larv.dis$PlantSpecies, permutations = 10000)
+larv.ad$aov.tab
+
+#adonis time, Composta only
+meta_composta<-meta[which(meta$Soil == 'Composta'),]
+otu_rare2<-otu_rare[row.names(otu_rare) %in% meta_composta$SampleID,]
+
+larv.dis<-vegdist(otu_rare2, method='bray')
+larv.dis<-as.data.frame(as.matrix(larv.dis))
+larv.dis$SampleID<-row.names(larv.dis)
+larv.dis<-merge(larv.dis, meta_composta[,c(1,5)], by= 'SampleID', all.y=F)
+larv.dis<-larv.dis[,-1]
+larv.permanova<-adonis(larv.dis[,-15] ~ larv.dis$PlantSpecies, permutations = 10000)
+larv.permanova$aov.tab
+
